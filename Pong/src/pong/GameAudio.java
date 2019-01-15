a46d3ddd31fd0db60da9b5c40c5e3eae87ae0930
Dome <Dome>
2019-01-15 12:21:11 +0100

    Sond fixed maybee it works

--- Pong/src/pong/GameAudio.java
+++ Pong/src/pong/GameAudio.java
@@ -25,40 +25,65 @@ public class GameAudio {
     DataLine.Info info;
     Clip clip;
 
-    public void playSound(File file, int volume) {
+    public void playSound(File file, Float volume) {
         try {
             stream = AudioSystem.getAudioInputStream(file);
             format = stream.getFormat();
             info = new DataLine.Info(Clip.class, format);
             clip = (Clip) AudioSystem.getLine(info);
             clip.open(stream);
+            //clip.loop(Clip.LOOP_CONTINUOUSLY);
             FloatControl.Type controlType;
-            if(clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+            if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
                 controlType = FloatControl.Type.MASTER_GAIN;
             } else {
                 controlType = FloatControl.Type.VOLUME;
             }
             FloatControl audioVolume = (FloatControl) clip.getControl(controlType);
-            audioVolume.setValue((audioVolume.getMaximum() - audioVolume.getMinimum()) / 100 * volume);
+            //audioVolume.setValue((audioVolume.getMaximum() - audioVolume.getMinimum()) / 100 * volume);
+            audioVolume.setValue(volume);
             clip.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
-    public File getFile (String filename) {
+
+    public void playMusic(File file, Float volume) {
+        try {
+            stream = AudioSystem.getAudioInputStream(file);
+            format = stream.getFormat();
+            info = new DataLine.Info(Clip.class, format);
+            clip = (Clip) AudioSystem.getLine(info);
+            clip.open(stream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            FloatControl.Type controlType;
+            if (clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+                controlType = FloatControl.Type.MASTER_GAIN;
+            } else {
+                controlType = FloatControl.Type.VOLUME;
+            }
+            FloatControl audioVolume = (FloatControl) clip.getControl(controlType);
+            //audioVolume.setValue((audioVolume.getMaximum() - audioVolume.getMinimum()) / 100 * volume);
+            audioVolume.setValue(volume);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getFile(String filename) {
         return new File("sounds" + separator + filename);
     }
 
     public void soundGameOver() {
-        playSound(getFile("go.wav"), 100);
+        playSound(getFile("go.wav"), -0.0f);
     }
 
     public void soundBall() {
-        playSound(getFile("ball.wav"), 100);
+        playSound(getFile("ball.wav"), -0.0f);
     }
 
     public void mainTheme() {
-        playSound(getFile("music3.wav"), 40);
+        playMusic(getFile("music3.wav"), -20.0f);
     }
 }
